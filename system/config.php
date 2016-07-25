32f3be3ffe2132d85920fa34d3501cf4fa4dbd7b
Rilong <romgnatyuk@gmail.com>
2016-07-25 18:36:22 +0300

    Added in .gitignore file  folder system

--- system/config.php
+++ system/config.php
@@ -2,10 +2,10 @@
 
 return array(
    // Database connect
-   "db_host" => "localhost",
-   "db_user" => "root",
-   "db_password" => "",
-   "db_name" => "manage",
+   "db_host" => "hnatyuk.mysql.ukraine.com.ua",
+   "db_user" => "hnatyuk_db",
+   "db_password" => "t24eHVNk",
+   "db_name" => "hnatyuk_db",
     
    "secret" => "frer456ttrf47" 
 );
